adb75f82ec3798ce928dbb146b1ee7be2f3abaf9
davidescapolan01 <davidescapolan01@gmail.com>
2024-02-01 21:21:10 +0100

    Inatallazione_&_creazione_progetto 00:23:38
    Prossimo a lezione Variabili

--- bin/corso_dart.dart
+++ bin/corso_dart.dart
@@ -1,4 +1,4 @@
-import 'package:corso_dart/corso_dart.dart' as corso_dart;
+import 'package:corso_dart/corso_dart.dart' as corso_dart; //package:  gli dice di cercare nella cartella ...\lib\
 
 void main(List<String> arguments) {
   print('Hello world: ${corso_dart.calculate()}!');
